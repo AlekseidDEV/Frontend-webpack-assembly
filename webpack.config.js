b522ec9500e1e69476ceb80760a38b5aa6459d86
Aleksei__developer <84816879+AlekseidDEV@users.noreply.github.com>
2023-09-20 19:22:34 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
     entry:{
         index: path.resolve(__dirname, 'src' , 'index.js'),
         // about: path.resolve(__dirname, 'src' , 'about.js'), тест
+        //
     },
     output:{
         filename: `./js/${filename('js')}`,
